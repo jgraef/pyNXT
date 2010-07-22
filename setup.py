403aa62bdf87f0866a11703672b6ad33559b8460
Janosch Gräf <janosch.graef@gmx.net>
2010-07-23 01:06:22 +0200

    - Added setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name = "pyNXT",
+      version = "0.10",
+      description = "This module provides access to objects and functions used to control a LEGO Mindstorms NXT",
+      author = "Janosch Gräf",
+      author_email = "janosch.graef@gmx.net",
+      url = "http://janosch-graef.org/pyanxt",
+      packages = ["anxt"],
+      license = "GNU GPL v3")
