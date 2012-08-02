e26970064ad1f7d34941c02ee50a0dbbad227668
Janosch Gräf <janosch@Enternix.(none)>
2012-08-02 19:56:11 +0200

    worked on nxtcam

--- anxt/CameraSensor.py
+++ anxt/CameraSensor.py
@@ -15,13 +15,89 @@
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from io import IOBase
+
 from .I2C import DEFAULT_I2C_ADDR
 from .Libanxt import Libanxt
 from .Sensor import DEFAULT_DIGITAL_PORT, DigitalSensor
-from ctypes import byref
+from ctypes import byref, Structure, c_ubyte
 
+# TODO split from C Colormap structure and put C structure into Libanxt (already is)
+x = """
+class Colormap(Structure):
+    FILE_SIG = b"#aNXTCam COLORMAP\n"
+    
+    _fields_ = [("r", (16 * c_ubyte)),
+                ("g", (16 * c_ubyte)),
+                ("b", (16 * c_ubyte))]
 
-# TODO: set/get, load/store, manipulate colormap (implement as class)
+    @staticmethod
+    def load(file_or_path):
+        if (isinstance(file_or_path, IOBase)):
+            f = file_or_path
+        else:
+            f = open(file_or_path, "rb")
+
+        sig = f.readline()
+        if (sig!=Colormap.FILE_SIG):
+            raise ColormapError("Invalid signature")
+
+        r = tuple(f.read(16))
+        g = tuple(f.read(16))
+        b = tuple(f.read(16))
+
+        if (f!=file_or_path):
+            f.close()
+
+        return Colormap(r, g, b)
+
+    def save(self, file_or_path):
+        if (isinstance(file_or_path, IOBase)):
+            f = file_or_path
+        else:
+            f = open(file_or_path, "wb")
+
+        f.write(self.FILE_SIG)
+
+        f.write(bytes(self.r))
+        f.write(bytes(self.g))
+        f.write(bytes(self.b))
+
+        if (f!=file_or_path):
+            f.close()
+
+    def get_r(self, i):
+        return (self.r[i*2]<<8) | self.r[i*2+1]
+
+    def get_g(self, i):
+        return (self.g[i*2]<<8) | self.g[i*2+1]
+
+    def get_b(self, i):
+        return (self.b[i*2]<<8) | self.b[i*2+1]
+
+    def get_average_color(self, i):
+        r = self.get_r(i)
+        g = self.get_g(i)
+        b = self.get_b(i)
+
+        r_sum = r_n = 0
+        g_sum = g_n = 0
+        b_sum = b_n = 0
+
+        for i in range(16):
+            r_sum += ((r>>(16-i))&1)*i*16
+            r_n += 1
+            g_sum += ((g>>(16-i))&1)*i*16
+            g_n += 1
+            b_sum += ((b>>(16-i))&1)*i*16
+            b_n += 1
+
+        return (r_sum/r_n,
+                g_sum/g_n,
+                b_sum/b_n)
+"""
+    
+
 
 class CameraSensor(DigitalSensor):
     tracking_modes = {"OBJECT": 0x42,
@@ -71,7 +147,7 @@ class CameraSensor(DigitalSensor):
 
     def enable_tracking(self, enable = True):
         self.set_addr_param("cam")
-        self.nxt.libanxt.nxt_cam_enable_tracking(self.nxt.handle, self.port-1, 1 if enable else 0)
+        self.nxt.libanxt.nxt_cam_enable_tracking(self.nxt.handle, self.port-1, int(enable))
 
     def set_trackingmode(self, mode = "OBJECT"):
         self.set_addr_param("cam")
@@ -81,6 +157,6 @@ class CameraSensor(DigitalSensor):
         self.set_addr_param("cam")
         self.nxt.libanxt.nxt_cam_reset(self.nxt.handle, self.port-1)
 
-    def nxt_cam_enable_colorsort(self, enable = True):
+    def enable_colorsort(self, enable = True):
         self.set_addr_param("cam")
-        self.nxt.libanxt.nxt_cam_enable_colorsort(self.nxt.handle, self.port-1, 1 if enable else 0)
+        self.nxt.libanxt.nxt_cam_enable_colorsort(self.nxt.handle, self.port-1, int(enable))
